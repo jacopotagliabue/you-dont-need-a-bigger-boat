63b93ce6ae5f66c248e6988f5745f3d2b07fd337
jacopo tagliabue <jacopo.tagliabue@tooso.org>
2021-05-26 17:59:14 -0400

    print out bunch of debug things for AWS Batch

--- flow_playground.py
+++ flow_playground.py
@@ -11,24 +11,40 @@ class DummyFlow(FlowSpec):
     @step
     def start(self):
         """
-        Create a random dataset
+        Create a random dataset.
         """
         print("flow name: %s" % current.flow_name)
         print("run id: %s" % current.run_id)
         print("username: %s" % current.username)
         import random
         self.dataset = [[random.uniform(0, 1.0), random.uniform(0.2, 0.9)] for _ in range(100000)]
+        self.next(self.prepare_dataset)
+
+    @step
+    def prepare_dataset(self):
+        """
+        Divide data point in train and test.
+        """
+        d = self.dataset
+        train_size = int(len(d) * 0.8)
+        self.train_dataset = [_ for idx, _ in enumerate(d) if idx <= train_size]
+        self.test_dataset = [_ for idx, _ in enumerate(d) if idx > train_size]
+        print("Training size: {}, Test size: {}".format(len(self.train_dataset), len(self.test_dataset)))
         self.next(self.train_model)
 
-    @batch(gpu=1, image='763104351884.dkr.ecr.us-east-1.amazonaws.com/tensorflow-training:2.3.1-gpu-py37-cu110-ubuntu18.04')
+    @batch(gpu=1, cpu=8, image='763104351884.dkr.ecr.us-east-1.amazonaws.com/tensorflow-training:2.3.1-gpu-py37-cu110-ubuntu18.04')
     @step
     def train_model(self):
         """
-        Train a regression model and use s3 client from metaflow to store the model tar file.
+        Train a regression model on the random dataset, on GPU.
         """
         import numpy as np
         import tensorflow as tf
         from tensorflow.keras import layers
+        from tensorflow.python.client import device_lib
+        print("Print out sytem device...")
+        print(device_lib.list_local_devices())
+        print("Starting GPU training now...")
         # build the model
         x_train = np.array([[_[0]] for _ in self.train_dataset])
         y_train = np.array([_[1] for _ in self.train_dataset])
