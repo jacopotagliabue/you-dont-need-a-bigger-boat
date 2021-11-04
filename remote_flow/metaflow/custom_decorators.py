fd5ac979ba2af904a15bdcd0cb3828817d791c41
Jeff Spencer <JeffSpencerD@gmail.com>
2021-11-04 17:04:21 -0500

    only pip install packages if running on batch

--- remote_flow/metaflow/custom_decorators.py
+++ remote_flow/metaflow/custom_decorators.py
@@ -5,12 +5,20 @@ def pip(libraries):
     def decorator(function):
         @wraps(function)
         def wrapper(*args, **kwargs):
+            import os
             import subprocess
             import sys
 
-            for library, version in libraries.items():
-                print('Pip Install:', library, version)
-                subprocess.run([sys.executable, '-m', 'pip', 'install', '--quiet', library + '==' + version])
+            IS_AWS_BATCH = os.getenv("AWS_BATCH_JOB_ID", None)
+
+            if IS_AWS_BATCH:
+                for library, version in libraries.items():
+                    print("Pip Install:", library, version)
+                    if version != "":
+                        subprocess.run([sys.executable, "-m", "pip", "install", library + "==" + version])
+                    else:
+                        subprocess.run([sys.executable, "-m", "pip", "install", library])
+
             return function(*args, **kwargs)
 
         return wrapper
